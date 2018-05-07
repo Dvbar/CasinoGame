331b7b125b985d2976a6dcf042207f2f801f86e0
JorHernandezdan <jordan3397@gmail.com>
2018-05-06 20:54:19 -0700

    card class early work

--- Blackjack_GUI.java
+++ Blackjack_GUI.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-public class gui_ttt {
+/*public class gui_ttt {
 	public static void main(String[]args) {
 		JFrame f = new JFrame();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,3 +24,27 @@ public class gui_ttt {
 		b.setBounds();
 	}
 }
+*/
+
+package ??
+import java fx.scene.Parent;
+//
+////
+///
+//
+//
+//
+
+public class Card extends Parent {
+
+	enum Suit {
+		SPADES, DIAMONDS, CLUBS, HEARTS
+	};
+	enum Rank {
+		TWO = 1, THREE = 3, FOUR = 4, FIVE = 5, SIX = 6, SEVEN = 7, EIGHT = 8, NINE = 9, TEN = 10, JACK = 10, QUEEN = 10, KING = 10, ACE = 11;
+		final int value;
+		private Rank (int value) {
+			this.value - value;
+		}
+	};
+
